c277890bbe5c816b3567498c0c1de1af3bb79c7f
benthehorn <benhorn13@googlemail.com>
2015-03-03 10:34:21 +0100

    added print line

--- GitShit/src/gitshit/GitShit.java
+++ GitShit/src/gitshit/GitShit.java
@@ -16,6 +16,7 @@ public class GitShit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ben has altered this.");
     }
     
 }
